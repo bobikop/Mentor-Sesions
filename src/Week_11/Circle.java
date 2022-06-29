bd43d6172e36739a759a995a5fc0fb081218fb83
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-06-28 20:49:31 -0400

    Repeat

---
+++ src/Week_11/Circle.java
@@ -0,0 +1,63 @@
+package Week_11;
+
+public class Circle {
+
+    //define two fields radius(necessary for all calculations)
+    private double radius;
+    //and numberOfCircle(keep the info of how many object was created from this class).
+    private static int numberOfCircle;
+
+    public static double pi;
+
+    //create default and parameterized constructors
+    //default constructor should increase the number of object when a new object created
+    public Circle(){
+        numberOfCircle++;
+    }
+
+    //parameterized constructor should increase the number of object when a new object created.
+    //and not allowed to initialize object with the radius value lower than Zero.
+
+    public Circle(double radius) {
+        setRadius(radius);
+    }
+
+    //create getter and setter methods for radius.
+    //getter
+    public double getRadius() {
+        return radius;
+    }
+
+    //setter if the value of radius is lower than zero keep the previous value.
+    public void setRadius(double radius) {//we defined the constraints and make a detailed contract with user.
+        if (radius < 0) {
+            System.out.println("Invalid radius input! Radius can not be negative number!");
+            System.exit(1);
+        }
+        this.radius = radius;
+    }
+
+    //do not allow any object to change value of static variable just get the current
+    //value of numberOfCircle. We did not allow to the user changing the value of this static field.
+    //it can just be changed while creating objects.
+    public static int getNumberOfCircle() {
+        return numberOfCircle;
+    }
+
+    //calculate area of Circle
+    public double calcArea() {
+        return (this.radius*this.radius*pi);
+    }
+
+    //calculate perimeter of Circle
+    public double calculatePerimeter() {
+        return radius*2*pi;
+    }
+
+
+}
+
+
+
+
+
---
+++ src/Week_11/TestCircleObjects.java
@@ -0,0 +1,32 @@
+package Week_11;
+
+public class TestCircleObjects {
+    public static void main(String[] args) {
+
+
+        Circle circle1 = new Circle(5);
+        System.out.println(circle1.getRadius());
+        circle1.setRadius(8.5);
+        circle1.calcArea();
+        System.out.println(circle1.calcArea());
+        System.out.println(circle1.calculatePerimeter());
+        System.out.println(circle1.getRadius()); // getRadius method call through object
+        System.out.println("-----------------------------------------------------------");
+        System.out.println(Circle.getNumberOfCircle());// static method call from class
+
+
+        Circle circle2 = new Circle(3.9);
+        System.out.println(circle2.getRadius());
+        circle2.setRadius(21);
+        circle2.setRadius(5);
+        System.out.println(circle2.calcArea());
+        System.out.println(circle2.calculatePerimeter());
+        System.out.println(circle2.getRadius()); // getRadius method call through object
+        System.out.println("-----------------------------------------------------------");
+        System.out.println(Circle.getNumberOfCircle());// static method call from class
+
+
+
+
+    }
+}
